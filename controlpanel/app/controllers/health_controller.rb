d2d15df7d470d1be289953f464a6787b38278a3c
Dharini Murugaprabhu <dharinishree@gmail.com>
2017-06-12 07:28:45 -0700

    Add health handler and test

--- controlpanel/app/controllers/health_controller.rb
+++ controlpanel/app/controllers/health_controller.rb
@@ -1,2 +1,25 @@
 class HealthController < ApplicationController
+    include HealthHelper
+
+    def index
+        # healthService method is available in helpers/health_helper
+        checks = {
+            linkerd: "http://localhost:9990/admin/ping",
+            namerd: "http://localhost:9991/admin/ping",
+            linkerdtcp: "http://localhost:9992/metrics",
+            linkerdviz: "http://localhost:3000",
+        }
+    
+        healthCheckResult = {
+          linkerd: true,
+          namerd: true,
+          linkerdtcp: true,
+          linkerdviz: true
+        }
+
+        respond_to do |format|
+            format.json  { render :json => healthCheckResult }
+            format.html  { render :html => "Please curl as json curl -i -H \"accept-encoding: txt/json\" http://localhost:1234/health" }
+        end
+    end
 end
