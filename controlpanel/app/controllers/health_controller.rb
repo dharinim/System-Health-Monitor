72159e01b7e856524f815b367b38191c0244e7b1
Dharini Murugaprabhu <dharinishree@gmail.com>
2017-06-12 08:41:20 -0700

    Helath check completed

--- controlpanel/app/controllers/health_controller.rb
+++ controlpanel/app/controllers/health_controller.rb
@@ -2,20 +2,32 @@ class HealthController < ApplicationController
     include HealthHelper
 
     def index
-        # healthService method is available in helpers/health_helper
+       # healthService method is available in helpers/health_helper
         checks = {
             linkerd: "http://localhost:9990/admin/ping",
             namerd: "http://localhost:9991/admin/ping",
             linkerdtcp: "http://localhost:9992/metrics",
             linkerdviz: "http://localhost:3000",
         }
-    
-        healthCheckResult = {
-          linkerd: true,
-          namerd: true,
-          linkerdtcp: true,
-          linkerdviz: true
-        }
+
+        mutex = Mutex.new        
+        healthCheckResult = {}
+
+        threads = []
+        for check in checks.keys()
+            threads << Thread.new(check) do |check|
+                health_check_url = checks[check]
+                status = healthService.healthCheck(url=health_check_url)
+                mutex.synchronize {
+                    healthCheckResult[check] = status    
+                    Thread.exit
+                }
+            end
+        end
+        
+        for thread in threads
+            thread.join
+        end
 
         respond_to do |format|
             format.json  { render :json => healthCheckResult }
